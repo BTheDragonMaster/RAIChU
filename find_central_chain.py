4525d1c00037b27d691b1155f68580d9e8691dc3
sophie.vromans <sophie.vromans@wur.nl>
2022-02-03 17:48:38 +0100

    Fixed big bug in find_central_chain (polyketides with carbon terminus)

--- find_central_chain.py
+++ find_central_chain.py
@@ -1,6 +1,7 @@
 from pks_elongation_reactions import *
 from pikachu.reactions.functional_groups import find_atoms, GroupDefiner
 
+
 POLYKETIDE_S = GroupDefiner('Sulphur atom polyketide', 'SC(C)=O', 0)
 
 def find_central_chain(polyketide):
@@ -28,6 +29,7 @@ def find_central_chain(polyketide):
             central_chain += [chain_carbon]
             visited.append(chain_carbon)
             while not end_carbon:
+                print(central_chain)
                 for next_atom in chain_carbon.neighbours:
                     ethyl_branch = False
                     methyl_group = False
@@ -51,16 +53,39 @@ def find_central_chain(polyketide):
                                     types.append(neighbour.type)
                             if types.count('H') == 4 and types.count('C') == 4:
                                 ethyl_branch = True
+                            visited.append(chain_carbon)
+
 
                         #Carbon of terminal carboxylic acid group is the final
                         #carbon in the central chain
                         if next_atom_neighbour_types.count('O') == 2:
-                            central_chain.append(next_atom)
+                            central_chain.append(next_atom) or next_atom_neighbour_types.count('H') == 3
                             end_carbon = True
+                            visited.append(chain_carbon)
+
 
                         #Confirm carbon doesn't belong to methyl sidebranch
                         if next_atom_neighbour_types.count('H') == 3:
                             methyl_group = True
+                            print('methyl branch!!')
+                        #If the methylgroup is terminal, it is not a sidebranch!!!
+                            count_c_neighbours_not_visited = 0
+                            for neighbouring_atom in chain_carbon.neighbours:
+                                if neighbouring_atom.type == 'C' and neighbouring_atom not in visited:
+                                    count_c_neighbours_not_visited += 1
+                            if count_c_neighbours_not_visited < 2:
+                                methyl_group = False
+                                visited.append(chain_carbon)
+                                for neighbour in chain_carbon.neighbours:
+                                    if neighbour.type == 'C' and neighbour not in visited:
+                                        next_neighbours = []
+                                        for next_neighbour in neighbour.neighbours:
+                                            next_neighbours.append(next_neighbour.type)
+                                        if next_neighbours.count('H') == 3:
+                                            central_chain.append(neighbour)
+                                end_carbon = True
+
+                            visited.append(chain_carbon)
 
                         #Identification of central chain carbon atoms through
                         #exclusion
@@ -69,7 +94,7 @@ def find_central_chain(polyketide):
                             central_chain += [next_atom]
                             chain_carbon = next_atom
                             visited.append(chain_carbon)
-
+    print(central_chain)
     return list(reversed(central_chain))
 
 
@@ -84,5 +109,5 @@ if __name__ == "__main__":
     struct2 = Smiles('C(C(=CCC(CC(CC(=O)O)=O)O)C)(=O)S').smiles_to_structure()
     #print(struct.graph)
     #Drawer(struct)
-    print(struct2.graph)
+
 

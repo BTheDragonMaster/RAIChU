51e296b91fbcb11fa7438b3538c9c18965cb1f0a
sophie.vromans <sophie.vromans@wur.nl>
2022-02-04 14:23:51 +0100

    Optimised make_nrp (take from SMILES dict instead of Structures dict)

--- NRPS_condensation.py
+++ NRPS_condensation.py
@@ -6,9 +6,9 @@ from pk_attach_to_domain import attach_to_domain_nrp
 from raichu_drawer import Drawer
 from find_central_peptide_chain import find_central_chain_nrp
 
-LEAVING_OH_BOND = BondDefiner('Leaving -OH group bond', 'C(=O)(O)C[N]', 0, 2)
+LEAVING_OH_BOND = BondDefiner('Leaving -OH group bond', 'C(=O)(O)CN', 0, 2)
 N_AMINO_ACID = GroupDefiner('Nitrogen atom amino acid', 'NCC(=O)O', 0)
-LEAVING_S_BOND = BondDefiner('Leaving -OH group bond', 'C(=O)(CN)S', 0, 4)
+
 def condensation_nrps(amino_acid, nrp_intermediate):
     """
     Returns the NRPS condensation product as a PIKAChU Structure object
@@ -17,7 +17,12 @@ def condensation_nrps(amino_acid, nrp_intermediate):
     amino_acid: PIKAChU Structure object of the amino acid
     """
     # Define the bond attached to the -OH leaving group
+    # nrp_intermediate.set_connectivities()
+    # nrp_intermediate.find_cycles()
+    # nrp_intermediate.refresh_structure()
+    # nrp_intermediate.set_atom_neighbours()
     found_bonds = find_bonds(LEAVING_OH_BOND, nrp_intermediate)
+    print(found_bonds, 'found bonds')
     assert len(found_bonds) == 1
     oh_bond = found_bonds[0]
 
@@ -35,7 +40,11 @@ def condensation_nrps(amino_acid, nrp_intermediate):
 
     # Carry out condensation reaction using build-in PIKAChU function
     condensation_product = condensation(nrp_intermediate, amino_acid, oh_bond, h_bond)[0]
-    condensation_product.find_cycles()
+    nrp_intermediate.set_connectivities()
+    nrp_intermediate.find_cycles()
+    nrp_intermediate.refresh_structure()
+    nrp_intermediate.set_atom_neighbours()
+    print('ja')
     return condensation_product
 
 def make_nrp(list_amino_acids):
@@ -52,19 +61,19 @@ def make_nrp(list_amino_acids):
         line = line.strip()
         name, smiles = line.split()
         name = name.upper()
-        structure_aa = Smiles(smiles).smiles_to_structure()
-        structure_aa.find_cycles()
-        dict_aa_structure[name] = structure_aa
+        # structure_aa = Smiles(smiles).smiles_to_structure()
+        # structure_aa.find_cycles()
+        dict_aa_structure[name] = smiles
 
     # Make every name in amino acid list all caps
     for i in range(len(list_amino_acids)):
         list_amino_acids[i] = list_amino_acids[i].upper()
 
     # Take amino acid Structure object from dict and add to growing NRP chain
-    nrp_chain_intermediate = dict_aa_structure[list_amino_acids[0]].copy()
+    nrp_chain_intermediate = Smiles(dict_aa_structure[list_amino_acids[0]]).smiles_to_structure()
     list_amino_acids = list_amino_acids[1:]
     for amino_acid_name in list_amino_acids:
-        amino_acid_struct = dict_aa_structure[amino_acid_name].copy()
+        amino_acid_struct = Smiles(dict_aa_structure[amino_acid_name]).smiles_to_structure()
         print(amino_acid_name)
         nrp_chain_intermediate = condensation_nrps(amino_acid_struct, nrp_chain_intermediate)
 
@@ -81,5 +90,6 @@ if __name__ == "__main__":
     test_peptide = make_nrp(['alanine', 'valine', 'tyrosine', 'citrulline',  'threonine', 'cysteine', 'norcoronamicacid', '(2S,3R)-2-amino-3-hydroxy-4-(4-nitrophenyl)butanoate'])
     attached_test_peptide = attach_to_domain_nrp(test_peptide, 'PCP')
     Drawer(attached_test_peptide)
-    print(find_central_chain_nrp(attached_test_peptide))
-    print(len(find_central_chain_nrp(attached_test_peptide)))
+    test_peptide2 = make_nrp(['d-threonine', 'valine', 'cysteine'])
+    attached_test_peptide2 = attach_to_domain_nrp(test_peptide2, 'PCP')
+    Drawer(attached_test_peptide2)

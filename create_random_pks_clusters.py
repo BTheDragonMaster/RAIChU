5d306e31c28d674ec8eabd901d9bcd9833689524
sophie.vromans <sophie.vromans@wur.nl>
2022-02-16 19:18:21 +0100

    Add generator random NRPS clusters

--- create_random_pks_clusters.py
+++ create_random_pks_clusters.py
@@ -1,5 +1,7 @@
 import random
 from visualize_pks_cluster import *
+from pikachu.smiles.smiles import *
+from pikachu.chem.structure import *
 
 starter_units = ['SC(=O)CC', 'SC(CC(O)=O)=O', 'SC(CC(O)=O)=O',
                  'SC(C(C(O)=O)CC)=O', 'SC(C(C(O)=O)OC)=O', 'SC(C*)=O',
@@ -8,7 +10,7 @@ starter_units = ['SC(=O)CC', 'SC(CC(O)=O)=O', 'SC(CC(O)=O)=O',
 tailoring_domain_combinations = [[], ['KR'], ['KR', 'DH'], ['KR', 'DH', 'ER']]
 elongation_units = ['malonylcoa', 'methylmalonylcoa', 'pk', 'methoxymalonylacp', 'ethylmalonylcoa']
 
-def generate_random_cluster():
+def generate_random_pks_cluster():
     """
     
     """
@@ -23,10 +25,32 @@ def generate_random_cluster():
 
     return cluster
 
+def generate_random_nrps_cluster():
+    """
+
+    """
+    list_aa = ['2-AMINOADIPICACID', '2-AMINOBUTYRICACID', 'ANTICAPSIN', '2-AMINOISOBUTYRICACID', 'ALLO-ISOLEUCINE', 'ALANINE', 'ALPHA-METHYLCYSTEINE', 'ALPHA-AMINO-PHENYL-VALERICACID', 'ARGININE', 'ASPARAGINE', 'ASPARTICACID', 'ALLO-THREONINE', 'CAPREOMYCIDINE', 'CITRULLINE', 'CORONAMICACID', 'CYSTEINE', 'CYSTEICACID', 'D-2-AMINOADIPICACID', 'D-2-AMINOBUTYRICACID', 'D-ALLO-ISOLEUCINE', 'D-ALANINE', 'D-ALPHA-METHYLCYSTEINE', 'D-ALPHA-AMINO-PHENYL-VALERICACID', 'D-ARGININE', 'D-ASPARAGINE', 'D-ASPARTICACID', 'D-ALLO-THREONINE', 'D-CITRULLINE', 'D-CYSTEINE', 'D-CYSTEICACID', 'D-2,4-DIAMINOBUTYRICACID', '(2R,3R)-2,3-DIAMINOBUTYRICACID', 'D-3,5-DIHYDROXYPHENYLGLYCINE', 'D-2,3-DIAMINOPROPIONICACID', 'D-ENDURACIDIDINE', 'D-ALPHA-ETHYLNORVALINE', 'D-GLUTAMINE', 'D-GLUTAMICACID', 'D-HYDROXYPHENYLGLYCINE', 'D-HOMOPROLINE', 'D-HOMOSERINE', 'D-HOMOSERINELACTONE', 'D-HOMOTYROSINE', 'D-ISOLEUCINE', 'D-ISOVALINE', 'D-KYNURENINE', 'D-LEUCINE', 'D-LYSINE', 'D-METHYL-2-AMINOOCTANOICACID', 'D-ORNITHINE', 'D-PHENYLGLYCINE', 'D-PHENYLALANINE', 'D-PROLINE', 'D-PHOSPHINOTHRICIN', 'D-SERINE', 'D-TERT-LEU', 'D-THREONINE', 'D-TRYPTOPHAN', 'D-TYROSINE', 'D-VALINE', '2,4-DIAMINOBUTYRICACID', '(2S)-2,3-DIAMINOBUTYRICACID', 'DEHYDROALANINE', '2,3-DEHYDRO-TRYPTOPHAN', 'DEHYDRO-CYSTEINE', '3,5-DIHYDROXYPHENYLGLYCINE', 'D-DIHYDROXYPHENYLTHIAZOLGROUP', 'DIHYDROXYPHENYLTHIAZOLGROUP', '2,3-DIAMINOPROPIONICACID', 'ENDURACIDIDINE', 'ALPHA-ETHYLNORVALINE', 'GLUTAMINE', 'GLUTAMICACID', 'GLYCINE', 'HOMOARGININE', 'HOMOISOLEUCINE', 'HISTIDINE', 'HYDROXYPHENYLGLYCINE', 'HOMOPHENYLALANINE', 'HOMOPROLINE', 'HOMOSERINE', 'HOMOSERINELACTONE', 'HOMOTYROSINE', 'ISOLEUCINE', 'ISOVALINE', 'KYNURENINE', '(2S,3S)-2,3-DIAMINOBUTYRICACID', 'LEUCINE', 'LYSINE', 'METHYL-2-AMINOOCTANOICACID', 'METHIONINE', 'NORCORONAMICACID', 'NORVALINE', 'ORNITHINE', 'PHENYLGLYCINE', 'PHENYLALANINE', 'PROLINE', 'PHOSPHINOTHRICIN', 'R-CORONAMICACID', 'R-NORCORONAMICACID', 'SERINE', 'TERT-LEU', 'THREONINE', 'TRYPTOPHAN', 'TYROSINE', 'VALINE', 'DEHYDROBUTYRINE', 'BETA-ALANINE', 'BETA-LYSINE', '(2S,3R)-2-AMINO-3-HYDROXY-4-(4-NITROPHENYL)BUTANOATE', '2,3-DIAMINO-3-METHYL-PROPANOICACID', '4,5-DIHYDROXYORNITHINE', '4,5-DIHYDROXYHOMOTYROSINE', '3-HYDROXYGLUTAMINE', '4-BUTENYL-4-METHYLTHREONINE', '4-HYDROXYVALINE', '2-METHYLSERINE', '4,5-DEHYDROARGININE', '3-(3-PYRIDYL)-ALANINE', '3-METHYLTYROSINE', 'D-LYSERGICACID', 'N-ACETYLPHENYLALANINE', '3-HYDROXY-4-O-METHYL-5-METHYL-TYROSINE', '4-METHYLPROLINE', 'ISOLEUCINE', '3-[(1R,2R)-2-NITROCYCLOPROPYL]-L-ALANINE', 'DEHYDROBUTYRINE', '(R)BETA-TYROSINE', 'PIPECOLICACID', '2-AMINO-9,10-EPOXY-8-OXODECANOICACID', 'L-PIPERAZICACID', 'DEHYDROTYROSINE', 'BETA-HYDROXYCYCLOHEX‐2‐ENYLALANINE', '2-METHYL-BETA-ALANINE', 'GLYCOCYAMINE']
+    starter_module = ['module 1', 'starter_module_nrps', random.choice(list_aa)]
+    cluster = [starter_module]
+    last_elongation_domain_nr = random.randint(2, 8)
+    for i in range(2, last_elongation_domain_nr):
+        elongation_module = [f'module {i}', 'elongation_module_nrps', random.choice(list_aa)]
+        cluster.append(elongation_module)
+    terminator_module =  [f'module {random.randint(2, 8)+1}', 'terminator_module_nrps', random.choice(list_aa)]
+    cluster.append(terminator_module)
+
+    return cluster
+
+
 if __name__ == "__main__":
+    for i in range(10):
+        cluster = generate_random_nrps_cluster()
+        print(cluster)
+        draw_pks_cluster(cluster)
+
     #Pick a number (how many random clusters you want to generate and draw)
     times = 100
     for i in range(times):
-        generated_cluster = generate_random_cluster()
+        generated_cluster = generate_random_pks_cluster()
         print(generated_cluster)
-        pks_cluster_to_structure(generated_cluster, attach_to_acp=True)
+        draw_pks_cluster(generated_cluster)

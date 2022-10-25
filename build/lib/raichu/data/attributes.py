c6d5909a785655b492aa496bec05f14870a816d8
Terlouw, Barbara <barbara.terlouw@wur.nl>
2022-10-25 21:27:58 +0200

    Removed files not part of the project.

--- build/lib/raichu/data/attributes.py
+++
@@ -1,9 +0,0 @@
-ATTRIBUTES = ['in_central_chain', 'KR_ep_target', 'KR_red_target',
-              'latest_elongation_o', 'latest_elongation_methyl', 'DH_target',
-              'ER_target', 'domain_type', 'unknown_index', 'chiral_c_ep',
-              'n_atom_nmeth', 'leaving_oh_o', 'leaving_oh_h', 'c2_acid',
-              'terminal_c', 'terminal_o']
-
-ALL_PKS_ELONGATION_UNITS = ['malonylcoa', 'methylmalonylcoa',
-                            'methoxymalonylacp', 'ethylmalonylcoa', 'pk']
-

ed8a08d969780f075f4f265a90feeb05161006ff
Friederike Biermann <friederike@biermann-erfurt.de>
2023-05-02 08:57:43 +0200

    Reorganized tailoring reactions and tailoring enzymes

--- raichu/tailoring_enzymes.py
+++ raichu/tailoring_enzymes.py
@@ -6,40 +6,53 @@ from raichu.data.molecular_moieties import CO_BOND, CC_DOUBLE_BOND, PEPTIDE_BOND
 from pikachu.reactions.functional_groups import find_atoms, find_bonds, combine_structures, GroupDefiner
 @unique
 class TailoringEnzymeType(Enum):
+    
+    #Transferation
     METHYLTRANSFERASE = 1
     C_METHYLTRANSFERASE = 2
     N_METHYLTRANSFERASE = 3
     O_METHYLTRANSFERASE = 4
     HYDROXYLATION = 5
-    OXIDATIVE_BOND_FORMATION = 6
-    EPOXIDATION = 7
-    DOUBLE_BOND_REDUCTION = 8
-    DOUBLE_BOND_SHIFT = 9
-    PRENYLTRANSFERASE = 10
-    ACETYLTRANSFERASE = 11
-    ACYLTRANSFERASE = 12
-    AMINOTRANSFERASE = 13
-    DOUBLE_BOND_FORMATION = 14
-    KETO_REDUCTION = 15
-    ALCOHOLE_DEHYDROGENASE = 16
-    DEHYDRATASE = 17
-    DECARBOXYLASE = 18
-    MONOAMINE_OXIDASE = 19
-    HALOGENASE = 20
-    PEPTIDASE = 21
-    PROTEASE = 22
-    MACROLACTAM_SYNTHETASE = 23
-    CYCLODEHYDRATION = 24
-    THREONINE_SERINE_DEHYDRATASE = 25
-    LANTHIPEPTIDE_CYCLASE = 26
-    LANTHIONINE_SYNTHETASE = 27
-    THIOPEPTIDE_CYCLASE = 28
-    AMINO_ACID_EPIMERASE = 29
-    SPLICEASE = 30
-    ARGINASE = 31
+    EPOXIDATION = 6
+    PRENYLTRANSFERASE = 7
+    ACETYLTRANSFERASE = 8
+    ACYLTRANSFERASE = 9
+    AMINOTRANSFERASE = 10
+    HALOGENASE = 11
 
+    #Bond modification
+    DOUBLE_BOND_REDUCTION = 12
+    DOUBLE_BOND_SHIFT = 13
+    DOUBLE_BOND_FORMATION = 14
+    PEPTIDASE = 15
+    PROTEASE = 16
+
+    #Oxidoreduction
+    KETO_REDUCTION = 17
+    ALCOHOLE_DEHYDROGENASE = 18
+    MONOAMINE_OXIDASE = 19
+
+    #Elimination
+    DEHYDRATASE = 20
+    THREONINE_SERINE_DEHYDRATASE = 21
+    DECARBOXYLASE = 22
+    SPLICEASE = 23
+    ARGINASE = 24
+
+    #Cyclization
+    OXIDATIVE_BOND_FORMATION = 25
+    MACROLACTAM_SYNTHETASE = 26
+    CYCLODEHYDRATION = 27
+    LANTHIPEPTIDE_CYCLASE = 28
+    LANTHIONINE_SYNTHETASE = 29
+    THIOPEPTIDE_CYCLASE = 30
     
-    
+    #Epimerization
+    AMINO_ACID_EPIMERASE = 31
+
+
+
+
     @staticmethod
     def from_string(label: str) -> "TailoringEnzymeType":
         for value in TailoringEnzymeType:
@@ -179,7 +192,7 @@ class TailoringEnzyme:
                     continue
                 atom1 = atom[0] #only one atom is modified at a time
                 atom1 = structure.get_atom(atom1)
-                structure = remove_atom(atom1, structure)     
+                structure = remove_atom(atom1, structure)
         elif self.type.name == "DEHYDRATASE":
             for atoms in self.modification_sites:
                 if len(atoms) < 2:
@@ -201,7 +214,7 @@ class TailoringEnzyme:
                 atom1 = structure.get_atom(atom1)
                 if atom1.type != "N":
                     raise ValueError(f"Can not perform MONOAMINE_OXYDASE on atom {atom1}, since there is no nitrogen to be removed.")
-                structure = remove_atom(atom1, structure) 
+                structure = remove_atom(atom1, structure)
         elif self.type.name == "HALOGENASE":
             for atom in self.modification_sites:
                 if len(atom) == 0:
@@ -358,7 +371,7 @@ class TailoringEnzyme:
                 carbon = structure.get_atom(atom[0])
                 assert carbon.type == "C"
                 structure = change_chirality(carbon, structure)
-        
+
         elif self.type.name == "SPLICEASE":
             for atoms in self.modification_sites:
                 if len(atoms) != 2:
@@ -386,13 +399,13 @@ class TailoringEnzyme:
                 structure.add_atom('H', [nitrogen])
                 structure.refresh_structure(find_cycles=True)
 
-                
+
         return structure
 
 
 
 
-    
+
     def get_possible_sites(self, structure):
         possible_sites = []
         if self.type.name in ["HYDROXYLATION",]:
@@ -409,13 +422,13 @@ class TailoringEnzyme:
                 find_atoms_for_tailoring(structure, "O"))
             possible_sites.extend(
                 find_atoms_for_tailoring(structure, "S"))
-        
+
         elif self.type.name in ["EPOXIDATION", "DOUBLE_BOND_REDUCTION"]:
             peptide_bonds = find_bonds(
                 CC_DOUBLE_BOND, structure)
             for bond in peptide_bonds:
                 possible_sites.append(bond.neighbours)
-        
+
         elif self.type.name == "DOUBLE_BOND_FORMATION":
             peptide_bonds = find_bonds(
                 CC_SINGLE_BOND, structure)
@@ -438,14 +451,14 @@ class TailoringEnzyme:
         elif self.type.name == "KETO_REDUCTION":
             oxygens = find_atoms(KETO_GROUP, structure)
             possible_sites.extend([oxygen.get_neighbour("O") for oxygen in oxygens])
-        
+
         elif self.type.name == "ALCOHOLE_DEHYDROGENASE":
             possible_sites.extend(
                 find_atoms_for_tailoring(structure, "O"))
-        
+
         elif self.type.name == "DECARBOXYLASE":
             possible_sites.extend(find_atoms(C_CARBOXYL, structure))
-        
+
         elif self.type.name == "DEHYDRATASE":
             co_bonds = find_bonds(CO_BOND, structure)
             for co_bond in co_bonds:
@@ -455,7 +468,7 @@ class TailoringEnzyme:
                         for neighbouring_atom in neighbouring_bond.neighbours:
                             if neighbouring_atom != co_bond.get_neighbour("C") and neighbouring_atom.has_neighbour("H"):
                                 possible_sites.append(neighbouring_bond.neighbours)
-    
+
         elif self.type.name == "MONOAMINE_OXIDASE":
             n_atoms_with_one_h = find_atoms_for_tailoring(structure, "N")
             for n_atom in n_atoms_with_one_h:

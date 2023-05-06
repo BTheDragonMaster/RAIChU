7db291ce8c54564adf2578911dfbd3ffd841f471
Terlouw, Barbara <barbara.terlouw@wur.nl>
2023-05-06 13:11:23 +0200

    renamed parameter

--- raichu/antismash.py
+++ raichu/antismash.py
@@ -1,8 +1,8 @@
 
 
-def load_antismash_gbk(gbk_file, antismash_version=7.0):
+def load_antismash_gbk(gbk_file, version=7.0):
     raise NotImplementedError()
 
 
-def load_antismash_js(js_file, antismash_version=7.0):
+def load_antismash_js(js_file, version=7.0):
     raise NotImplementedError()

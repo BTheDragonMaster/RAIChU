d9fe908d861f587725dcf206c024b23af7983d1c
Friederike Biermann <friederike@biermann-erfurt.de>
2023-05-10 16:01:19 +0200

    Made terpene cyclization also work without prior reduction

--- raichu/cluster/terpene_cluster.py
+++ raichu/cluster/terpene_cluster.py
@@ -4,7 +4,7 @@ from pikachu.general import read_smiles
 
 from raichu.substrate import TerpeneCyclaseSubstrate
 from raichu.representations import MacrocyclizationRepresentation, TailoringRepresentation
-from raichu.reactions.general_tailoring_reactions import dephosphorylation, oxidative_bond_formation
+from raichu.reactions.general_tailoring_reactions import dephosphorylation, oxidative_bond_formation, double_bond_reduction
 from raichu.cluster.base_cluster import Cluster
 
 
@@ -36,6 +36,16 @@ class TerpeneCluster(Cluster):
         for macrocyclization_atoms, cyclisation_type in initialized_macrocyclization_atoms:
             atom1 = self.chain_intermediate.get_atom(macrocyclization_atoms[0])
             atom2 = self.chain_intermediate.get_atom(macrocyclization_atoms[1])
+            found_bond = False
+            for atom in [atom1, atom2]:
+                if found_bond and any([neighbour.type== "H" for neighbour in atom.neighbours]):
+                    break
+                for bond in atom.get_bonds():
+                    if bond.type == "double":
+                        self.chain_intermediate = double_bond_reduction(
+                            *bond.neighbours, self.chain_intermediate)
+                        found_bond = True
+                        break
             self.chain_intermediate = oxidative_bond_formation(atom1, atom2, self.chain_intermediate)
             self.cyclic_intermediates.append(self.chain_intermediate.deepcopy())
 

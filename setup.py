574f783a9f8eae686cd982199326ba46ac9605a8
Terlouw, Barbara <barbara.terlouw@wur.nl>
2024-04-15 18:11:26 +0200

    Added timeout decorator to dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.0.0"
-DESCRIPTION = "RAIChU"
-LONG_DESCRIPTION = (
-    "A package to automatically draw natural product biosynthesis pathways."
-)
+
+VERSION = '1.0.2'
+DESCRIPTION = 'RAIChU'
+LONG_DESCRIPTION = 'A package to automatically draw natural product biosynthesis pathways.'
 
 print(find_packages())
 
@@ -17,6 +16,9 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(exclude="build"),
     package_data={"": ["*.png", "*.svg", "*.txt"]},
-    install_requires=["matplotlib", "biopython==1.76", "pikachu-chem>=1.1.1"],
-    scripts=["bin/raichu"],
-)
+
+    install_requires=['matplotlib',
+                      'biopython==1.76',
+                      'pikachu-chem>=1.1.1',
+                      'timeout-decorator==0.5.0'],
+    scripts=["bin/raichu"],)

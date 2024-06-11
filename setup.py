ab39cb3774c307130302d0b846cc301ea76ce3ab
FriederikeBiermann <friederike@biermann-erfurt.de>
2024-06-11 11:48:06 +0200

    Removed syntax errors

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
-DESCRIPTION = 'RAIChU'
-LONG_DESCRIPTION = 'A package to automatically draw natural product biosynthesis pathways.'
+VERSION = "0.0.2"
+DESCRIPTION = "RAIChU"
+LONG_DESCRIPTION = (
+    "A package to automatically draw natural product biosynthesis pathways."
+)
 
 print(find_packages())
 
 setup(
     name="raichu",
     version=VERSION,
-    
     author="Barbara Terlouw",
     author_email="barbara.r.terlouw@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(exclude="build"),
     package_data={"": ["*.png", "*.svg", "*.txt"]},
-    install_requires=['matplotlib',
-                      'biopython==1.76',
-                      'pikachu-chem>=1.0.13',
-                      'paras'],
-    scripts=["bin/raichu"],)
+    python_requires=">=3.7, <3.10",
+    install_requires=["matplotlib", "biopython==1.76", "pikachu-chem>=1.0.13", "paras"],
+    scripts=["bin/raichu"],
+)

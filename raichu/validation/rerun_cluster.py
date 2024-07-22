9ca92bc738b25bbf3b1e790b33a34533191fb471
Terlouw, Barbara <barbara.terlouw@wur.nl>
2024-07-22 10:48:18 +0200

    Bugfix: parse antiSMASH domain names as domain name, not type

--- raichu/validation/rerun_cluster.py
+++ raichu/validation/rerun_cluster.py
@@ -1,5 +1,5 @@
 from raichu.representations import ClusterRepresentation
-from raichu.run_raichu import draw_cluster
+from raichu.run_raichu import draw_cluster, write_linear_product
 from raichu.general import draw_cluster_from_modular_cluster_representation, draw_products_from_modular_cluster_representation
 from sys import argv
 import os
@@ -12,9 +12,11 @@ if not os.path.exists(out_path):
     os.mkdir(out_path)
 
 out_pathway = os.path.join(out_path, "pathway.svg")
+out_smiles = os.path.join(out_path, "smiles.txt")
 
 draw_cluster_from_modular_cluster_representation(cluster, out_pathway)
 draw_products_from_modular_cluster_representation(cluster, out_path)
+write_linear_product(cluster, out_smiles)
 
 out_file = os.path.join(argv[1], "cluster.svg")
 

7301b531e2f748535cda3809dae8334cad5c7a1f
Terlouw, Barbara <barbara.terlouw@wur.nl>
2025-12-05 10:59:50 +0100

    Update RAIChU tests to reflect pikachu 1.1.4

--- raichu/test/integration/test_nrps.py
+++ raichu/test/integration/test_nrps.py
@@ -19,7 +19,7 @@ def test_NRPS():
     cluster.compute_structures(compute_cyclic_products=False)
     cluster.do_tailoring()
 
-    # Check if all tayloring reactions are performed
+    # Check if all tailoring reactions are performed
     for module in cluster.modules:
         for domain in module.tailoring_domains:
             if domain.supertype == "TAILORING":
@@ -28,7 +28,7 @@ def test_NRPS():
     # Check if final structure is correct
     assert (
         structure_to_smiles(cluster.chain_intermediate)
-        == r"OC(=O)[C@H](N(C(=O)[C@@H](NC(=O)[C@@H](NC(=O)[C@H](NC(=O)[C@@H](NC(=O)[C@H](NC(=O)[C@@H](NC(c6ccc(Cl)[nH]6)=O)C[C@@H]5C[C@@H]5[N+]([O-])=O)[C@H](O)C)[C@H](c4ccccc4)C)C[C@@H]2C[C@@H]2[N+]([O-])=O)[C@H](c3ccccc3)C)[C@H](C)CC)C1)C[C@H]1/C=C\C"
+        == r"OC(=O)[C@@H](N(C(=O)[C@@H](NC(=O)[C@@H](NC(=O)[C@H](NC(=O)[C@@H](NC(=O)[C@H](NC(=O)[C@@H](NC(c6ccc(Cl)[nH]6)=O)C[C@@H]5C[C@H]5[N+]([O-])=O)[C@H](O)C)[C@@H](c4ccccc4)C)C[C@@H]2C[C@H]2[N+]([O-])=O)[C@@H](c3ccccc3)C)[C@@H](C)CC)C1)C[C@@H]1/C=C\C"
     )
 
 
